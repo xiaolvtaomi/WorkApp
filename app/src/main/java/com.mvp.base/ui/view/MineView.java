b97de2fbfd335b5ab2b1d20e42e3d0b026f7a58f
wangzhiwei <1479423143@qq.com>
2017-07-20 17:30:06 +0800

    取消电话加密

--- app/src/main/java/com.mvp.base/ui/view/MineView.java
+++ app/src/main/java/com.mvp.base/ui/view/MineView.java
@@ -1,15 +1,23 @@
 package com.mvp.base.ui.view;
 
+import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.Toolbar;
 import android.util.AttributeSet;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.afollestad.materialdialogs.MaterialDialog;
 import com.jude.easyrecyclerview.EasyRecyclerView;
 import com.jude.easyrecyclerview.adapter.RecyclerArrayAdapter;
 import com.jude.easyrecyclerview.decoration.SpaceDecoration;
@@ -23,7 +31,9 @@ import com.mvp.base.model.bean.VideoInfo;
 import com.mvp.base.model.bean.VideoType;
 import com.mvp.base.presenter.VideoInfoPresenter;
 import com.mvp.base.presenter.contract.MineContract;
+import com.mvp.base.ui.activitys.AvatarAddActivity;
 import com.mvp.base.ui.activitys.CollectionActivity;
+import com.mvp.base.ui.activitys.DishAddActivity;
 import com.mvp.base.ui.activitys.HistoryActivity;
 import com.mvp.base.ui.activitys.SettingActivity;
 import com.mvp.base.ui.adapter.MineHistoryVideoListAdapter;
@@ -72,7 +82,12 @@ public class MineView extends RootView<MineContract.Presenter> implements MineCo
     TextView tvCollection;
     @BindView(R.id.tv_them)
     TextView tvThem;
-
+    TextView moible;
+    ImageView avatar2;
+    Button picture;
+    Button photo;
+    Button back;
+    Dialog dialog;
     public MineView(Context context) {
         super(context);
     }
@@ -88,6 +103,8 @@ public class MineView extends RootView<MineContract.Presenter> implements MineCo
 
     @Override
     protected void initView() {
+        moible= (TextView) findViewById(R.id.avatar_set);
+        avatar2= (ImageView) findViewById(R.id.avatar2);
         ((AppCompatActivity) getContext()).setSupportActionBar(toolbar);
         toolbar.setTitle("");
         titleName.setText(getResources().getString(R.string.mine_title));
@@ -95,7 +112,6 @@ public class MineView extends RootView<MineContract.Presenter> implements MineCo
         StringUtils.setIconDrawable(mContext, tvDown, MaterialDesignIconic.Icon.gmi_time_countdown, 16, 15);
         StringUtils.setIconDrawable(mContext, tvCollection, MaterialDesignIconic.Icon.gmi_collection_bookmark, 16, 15);
         StringUtils.setIconDrawable(mContext, tvThem, MaterialDesignIconic.Icon.gmi_palette, 16, 15);
-
         mRecyclerView.setAdapter(mAdapter = new MineHistoryVideoListAdapter(mContext));
         GridLayoutManager gridLayoutManager = new GridLayoutManager(mContext, 3);
         gridLayoutManager.setSpanSizeLookup(mAdapter.obtainGridSpanSizeLookUp(3));
@@ -105,6 +121,12 @@ public class MineView extends RootView<MineContract.Presenter> implements MineCo
         itemDecoration.setPaddingStart(true);
         itemDecoration.setPaddingHeaderFooter(false);
         mRecyclerView.addItemDecoration(itemDecoration);
+        avatar2.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+             showDialog();
+            }
+        });
     }
 
     @Override
@@ -144,7 +166,7 @@ public class MineView extends RootView<MineContract.Presenter> implements MineCo
     }
 
 
-    @OnClick({R.id.rl_record, R.id.rl_down, R.id.rl_collection, R.id.rl_them, R.id.img_setting})
+    @OnClick({R.id.rl_record, R.id.rl_down, R.id.rl_collection, R.id.rl_them, R.id.img_setting })
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.rl_record:
@@ -181,4 +203,36 @@ public class MineView extends RootView<MineContract.Presenter> implements MineCo
         EventBus.getDefault().unregister(this);
         super.onDetachedFromWindow();
     }
+
+    private  void showDialog() {
+        View view = LayoutInflater.from(getContext()).inflate( R.layout.photo_choose_dialog, null);
+         dialog = new Dialog(getContext(), R.style.transparentFrameWindowStyle);
+        dialog.setContentView(view, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.FILL_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT));
+        Window window = dialog.getWindow();
+        // 设置显示动画
+        window.setWindowAnimations(R.style.main_menu_animstyle);
+        final WindowManager.LayoutParams wl = window.getAttributes();
+        wl.x = 0;
+        wl.y = 1280;
+//        wl.y = getWindowManager().getDefaultDisplay().getHeight();
+        // 以下这两句是为了保证按钮可以水平满屏
+        wl.width = ViewGroup.LayoutParams.MATCH_PARENT;
+        wl.height = ViewGroup.LayoutParams.WRAP_CONTENT;
+        // 设置显示位置
+        dialog.onWindowAttributesChanged(wl);
+        // 设置点击外围解散
+        dialog.setCanceledOnTouchOutside(true);
+        picture= (Button)view.findViewById(R.id.picture);
+        photo= (Button)view.findViewById(R.id.photo);
+        back= (Button)view.findViewById(R.id.back);
+        back.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+            }
+        });
+        dialog.show();
+    }
+
 }
